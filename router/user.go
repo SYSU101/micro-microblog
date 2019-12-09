d38002baafbf6f3d3581cd078c98a9e0be527d72
爱玩传奇 <862238910@qq.com>
2019-12-09 21:22:11 +0800

    add put

--- router/user.go
+++ router/user.go
@@ -1,10 +1,5 @@
 package router
-type User_tem struct {
-	Name string `json:"name"`
-	StudentId int `json:"studentId"`
-	Motto string `json:"motto"`
-	Birthday string `json:"birthday"`
-}
+
 import (
 	"net/http"
 
