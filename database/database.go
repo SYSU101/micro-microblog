2ac42a5c29601d3b1648796cbf88ee41316c2cc9
penhison <penhison@gmail.com>
2019-12-09 18:27:57 +0800

    update database

--- database/database.go
+++ database/database.go
@@ -29,9 +29,9 @@ func StartDB(db_file_name string) {
 	//所有的bucket的键为id，id的键为username
 	db.Update(func(tx *bolt.Tx) error {
 		bucket_key := [8][]byte{
-			[]byte("sessionID"), []byte("id"), 
-			[]byte("username"), []byte("name"), 
-			[]byte("studentId"), []byte("motto"), 
+			[]byte("sessionID"), []byte("id"),
+			[]byte("username"), []byte("name"),
+			[]byte("studentId"), []byte("motto"),
 			[]byte("password"), []byte("brithday")}
 		for i := 0; i < 8; i++ {
 			_, err := tx.CreateBucketIfNotExists([]byte(bucket_key[i]))
@@ -103,9 +103,43 @@ func UserLogin(username, password string) (string, error){
 		}
 		new_uuid, _ := uuid.NewV4()
 		b_sessionID.Put(id, new_uuid.Bytes())
-		return nil		
+		return nil
 	})
 	return string(new_uuid), err
+}
+
+// 注册用户，用户存在则返回 errors.New("用户名已被使用")
+func UserRegister(username, name, password string) error {
+
+}
+
+// 编辑用户属性, sessionId 无用户时返回errors.New("用户未登录")
+func UserEditName(sessionID, name string) error {
+
+}
+
+// 编辑用户属性, sessionId 无用户时返回errors.New("用户未登录")
+func UserEditStudentId(sessionID, studentId string) error {
+
+}
+
+// 编辑用户属性, sessionId 无用户时返回errors.New("用户未登录")
+func UserEditBrithday(sessionID, brithday string) error {
+
+}
+
+// 编辑用户属性, sessionId 无用户时返回errors.New("用户未登录")
+func UserEditMotto(sessionID, motto string) error {
+
+}
+
+// 编辑用户属性, sessionId 无用户时返回errors.New("用户未登录")
+func UserEditPassword(sessionID, password string) error {
+
+}
+
+func UserEditInfo(sessionID, name, studentId, brithday, motto string) error {
+
 }
 
 func main() {
