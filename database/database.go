dfc71606e59e26d607e68d0c766887505be0a350
爱玩传奇 <862238910@qq.com>
2019-12-09 19:36:31 +0800

    database

--- database/database.go
+++ database/database.go
@@ -10,3 +10,9 @@ func DeleteSessionByID(sessionID string) error
 // 若用户名和密码均正确，则使用 uuid.NewV4() 创建一个新的 session 记录，并返回对应的 sessionID
 // 若该用户不存在或密码不正确，则返回 errors.New("用户名或密码错误")
 func UserLogin(username, password string) (sessionID string, err error)
+
+//得到所有的用户
+func GetAllUsers() []User
+
+//通过用户id得到user信息
+func GetUserByUserID(userId) (User ,error)
